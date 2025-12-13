a7f2100eb0212c4e553834c8f81f433355fa77ed
Deepanshu Chaid <bantar117@gmail.com>
2025-12-13 11:03:06 +0000

    error fixed

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -11,15 +11,11 @@ export default defineConfig({
     },
   },
   server: {
-    host: '0.0.0.0',
-    port: process.env.PORT || 3000,
-    allowedHosts: [
-      '9b4f0566-4d0a-496b-be4c-b9bd7cf152bd-00-3fxapm4mzv3wj.pike.replit.dev',
-    ],
+    host: "0.0.0.0",
+    port: 3000,
     fs: {
       strict: false,
     },
     // 👇 this line makes /app and nested routes load properly even after refresh
-    historyApiFallback: true,
   },
 });
