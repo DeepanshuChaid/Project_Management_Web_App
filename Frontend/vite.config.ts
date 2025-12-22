02ebf51e26090ea7a31410de6c7c79252a5f748f
Deepanshu Chaid <bantar117@gmail.com>
2025-12-22 11:46:40 +0000

    new frontend

---
+++ Frontend/vite.config.ts
@@ -0,0 +1,21 @@
+import path from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  server: {
+    host: "0.0.0.0",
+    port: 3000,
+    fs: {
+      strict: false,
+    },
+    // 👇 this line makes /app and nested routes load properly even after refresh
+  },
+});
